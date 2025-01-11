04615fd5a9ebfeef1dd4c94dbc06edd3f1f70836
skctrl25 <shivakumarctrl@gmail.com>
2025-01-11 20:36:11 +0530

    Time: 4 ms (94.60%) | Memory: 45.5 MB (45.32%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if (s.length() < k) return false;
+
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int oddFreq = 0;
+        for (int count : freq) {
+            if (count % 2 != 0) {
+                oddFreq++;
+            }
+        }
+
+        return oddFreq <= k;
+    }
+}
